2de55c465c26addd51441621b81c00fe62f4ee37
Jacobfis <jacobnf@live.dk>
2024-11-11 10:28:34 +0100

    API set up and migrated users to db

---
+++ api/Controllers/StatusController.cs
@@ -0,0 +1,35 @@
+﻿using API.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        private readonly AppDBContext _context;
+
+        public StatusController(AppDBContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]
+        public IActionResult GetStatus()
+        {
+            return Ok("The server is Live");
+        }
+
+        [HttpGet("DB")]
+        public IActionResult GetStatusDB()
+        {
+            if (_context.Database.CanConnect())
+            {
+                return Ok("The database and Server is Live!");
+            }
+            else return NotFound();
+        }
+    }
+}
---
+++ api/Models/Common.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class Common
+    {
+        [Key]
+        public String Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
