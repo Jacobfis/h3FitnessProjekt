a1022e45c6fdf00d257320d698f24a0aa70ce72b
Jacobfis <jacobnf@live.dk>
2024-11-15 08:13:34 +0100

    Test

--- api/Program.cs
+++ api/Program.cs
@@ -1,69 +1,75 @@
-using API.Context;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
 using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Text;
-using Microsoft.AspNetCore.DataProtection;
+using API.Context;
 
 namespace API
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Add 
+
             builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             builder.Services.AddDataProtection().UseCryptographicAlgorithms(
-                new AuthenticatedEncryptorConfiguration
-                {
-                    EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
-                    ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
-                });
+            new AuthenticatedEncryptorConfiguration
+            {
+                EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
+                ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
+            });
 
             IConfiguration Configuration = builder.Configuration;
+            Console.WriteLine("start");
             var Connection = Configuration.GetConnectionString("DefaultConnection") ?? Environment.GetEnvironmentVariable("DefaultConnection");
             var issuer = Configuration["JwtSettings:Issuer"] ?? Environment.GetEnvironmentVariable("Issuer");
             var audience = Configuration["JwtSettings:Audience"] ?? Environment.GetEnvironmentVariable("Audience");
             var key = Configuration["JwtSettings:Key"] ?? Environment.GetEnvironmentVariable("Key");
 
             string connectionString = Configuration.GetConnectionString("DefaultConnection")
-            ?? Environment.GetEnvironmentVariable("DefaultConnection");
+                                        ?? Environment.GetEnvironmentVariable("DefaultConnection");
 
             builder.Services.AddDbContext<AppDBContext>(options =>
-                    options.UseNpgsql(connectionString));
+            options.UseNpgsql(connectionString));
 
-            // Add CORS policy to allow any origin, method, and header
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("AllowAll",
                     builder =>
                     {
-                        builder.AllowAnyOrigin()   // Allow any origin
-                               .AllowAnyMethod()   // Allow any HTTP method (GET, POST, etc.)
-                               .AllowAnyHeader();  // Allow any headers
+                        builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader();
                     });
             });
 
             // Configure JWT Authentication
-            builder.Services.AddAuthentication(x =>
+            builder.Services.AddAuthentication(item =>
             {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(x =>
+                item.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                item.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                item.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(item =>
             {
-                x.TokenValidationParameters = new TokenValidationParameters
+                item.TokenValidationParameters = new TokenValidationParameters
                 {
-                    ValidIssuer = Configuration["JwtSettings:Issuer"] ?? Environment.GetEnvironmentVariable("Issuer"),
-                    ValidAudience = Configuration["JwtSettings:Audience"] ?? Environment.GetEnvironmentVariable("Audience"),
+                    ValidIssuer = Configuration["JwtSettings:Issuer"]
+                                    ?? Environment.GetEnvironmentVariable("Issuer"),
+                    ValidAudience = Configuration["JwtSettings:Audience"]
+                                      ?? Environment.GetEnvironmentVariable("Audience"),
                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtSettings:Key"] ?? Environment.GetEnvironmentVariable("Key"))),
                     ValidateIssuer = true,
                     ValidateAudience = true,
@@ -74,17 +80,21 @@ namespace API
 
             var app = builder.Build();
 
-            // Use the CORS policy defined earlier
-            app.UseCors("AllowAll");
+            // TODO: LAV SÅ DEN GIVER RIGTIG HEADER ISTEDET FOR AT TILLADE ALT
+            // Allows all headers for CORS
+            app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed(origin => true).AllowCredentials());
 
             // Configure the HTTP request pipeline.
             app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            app.UseHttpsRedirection();
 
-            //app.UseHttpsRedirection();
-            app.MapGet("/", () => Results.Ok("API is running"));
-
+            app.UseAuthentication();
             app.UseAuthorization();
+
 
             app.MapControllers();
 
